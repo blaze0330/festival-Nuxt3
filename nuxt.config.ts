826b87fbc156ad66534f1f076147cc0a457eda82
Blaze <jamesbakker0330@gmail.com>
2023-02-01 22:34:51 +0100

    images not found

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: true, // default value
+    target: static,
     modules: [
         '@nuxtjs/tailwindcss',
         '@nuxt/image-edge'
